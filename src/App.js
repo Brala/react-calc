bddad316a3bd594cae426ee08848d0d7babab2a3
Piotr Bralewski <elobralu@gmail.com>
2019-08-03 18:08:39 +0200

    RWD styles improved

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.stats={
+
+    }
+  }
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+        initializeds
+        </header>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
