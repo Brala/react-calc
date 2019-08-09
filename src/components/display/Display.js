e246731611f09be30f3323703aa2607dbe6f305d
Brala <elobralu@gmail.com>
2019-08-09 09:17:05 +0200

    displays update: dynamic size scaling

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import './Display.css';
 
+class AutoScalingText extends React.Component {
+  state = {
+    scale: 1
+  };
+  
+  componentDidUpdate() {
+    const { scale } = this.state
+    console.log(scale)
+    const node = this.node
+    const parentNode = node.parentNode
+    
+    const availableWidth = parentNode.offsetWidth
+    const actualWidth = node.offsetWidth
+    const actualScale = availableWidth / actualWidth
+    console.log(availableWidth)
+    console.log(actualWidth)
+    if (scale === actualScale)
+      return
+    
+    if (actualScale < 1) {
+      this.setState({ scale: actualScale })
+    } else if (scale < 1) {
+      this.setState({ scale: 1 })
+    }
+  }
+  
+  render() {
+    const { scale } = this.state
+    
+    return (
+      <div
+        className="auto-scaling-text"
+        style={{ transform: `scale(${scale},${scale})` }}
+        ref={node => this.node = node}
+      >{this.props.children}</div>
+    )
+  }
+}
+
 class Display extends Component {
   render() {
     return (
-      <input type="text" id="display" className={this.props.className} value={this.props.currentNumber} readOnly>
+      <div type="text" id="display" className={this.props.className}  readOnly>
         
-      </input>
+        <AutoScalingText>{this.props.currentNumber}</AutoScalingText>
+      </div>
     )
   }
 }
